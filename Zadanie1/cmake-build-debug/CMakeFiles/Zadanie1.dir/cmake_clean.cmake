65e4ff969255f2766b447de3e88fbdc3e83b523e
Patryk <gustaw5022@gmail.com>
2019-03-18 23:26:42 +0100

    tym razem poprawny init helloWorlda

---
+++ Zadanie1/cmake-build-debug/CMakeFiles/Zadanie1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zadanie1.dir/main.cpp.o"
+  "Zadanie1.pdb"
+  "Zadanie1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zadanie1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
