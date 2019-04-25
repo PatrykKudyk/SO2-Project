b7af675988576d775bd3973f653d4b35eacb09f8
Patryk <gustaw5022@gmail.com>
2019-04-25 16:42:28 +0200

    better logic - balls are created just before threads

--- SecondStage/src/window.cpp
+++ SecondStage/src/window.cpp
@@ -25,15 +25,16 @@ Window::~Window(){
 void Window::startWindow(){
 
     srand(time(NULL));
-    for(int i = 0; i < 200; i++){
-        balls.push_back(new Ball( 2, width/2, getRandomDirection()));
+  //  for(int i = 0; i < 200; i++){
+  //      balls.push_back(new Ball( 2, width/2, getRandomDirection()));
        // balls.push_back(new Ball( 2, width/2, i));
-    }
+  //  }
 
     std::vector<std::thread> threadVect;
 
 
-    for(int i = 0; i < balls.size(); i++){
+    for(int i = 0; i < balls.size() + 1; i++){
+        balls.push_back(new Ball( 2, width/2, getRandomDirection()));
         symbol = getch();
         if(symbol == 'q')
         {
